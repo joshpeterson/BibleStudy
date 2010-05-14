08390304d9cb6d5f8e35f53612a435f284270f8b
Josh Peterson <petersonjm1@gmail.com>
2010-05-14 07:36:04 -0400

    Finished parsing of KJV and starting parsing of RSV.

---
+++ Utilities/RSVTranslationParser/RSVParser.py
@@ -0,0 +1,41 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
+
+index = urllib2.urlopen("http://quod.lib.umich.edu/r/rsv/browse.html")
+indexSoup = BeautifulSoup(index)
+baseUrl = "http://quod.lib.umich.edu"
+verseNumber = 0
+
+bibleFile = open("RSV.txt", "w")
+
+bible = []
+
+for relativeBookUrl in indexSoup.findAll(href=re.compile("^/cgi.*")):
+    fullBookUrl = baseUrl + relativeBookUrl['href']
+    book = urllib2.urlopen(fullBookUrl)
+    bookSoup = BeautifulSoup(book)
+    lines = str(bookSoup).split("<br />")
+    header = lines[0].split("</h2>")[0]
+    
+    book = header.split("<h2>")[1]
+    print "Getting book: " + book
+
+    lines[0] = lines[0].split("</h2>")[1]
+    chapter = 0
+    for line in lines:
+        verseNumber = verseNumber + 1
+        if line.count("<h3>") > 0:
+            chapter = chapter + 1
+            verse = 0
+
+        verse = verse + 1
+        if line.count("]"):
+            verseText = line.split("] ")[1]
+
+            verseEntry = str(verseNumber) + "*" + book + "*" + str(chapter) + "*" + str(verse) + "*" + verseText
+            bible.append(verseEntry)
+
+for line in bible:
+    bibleFile.write(line + "\n")
+bibleFile.close()
--- Utilities/RSVTranslationParser/RSVTranslationParser.py
+++
@@ -1,1 +0,0 @@
-from BeautifulSoup import BeautifulSoup
