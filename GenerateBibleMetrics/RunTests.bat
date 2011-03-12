de7efffedcb4694f7be22869bec7f595d197f6ea
Josh Peterson <petersonjm1@gmail.com>
2011-03-12 08:10:40 -0500

    Generate a list of books in all translations.

--- GenerateBibleMetrics/RunTests.bat
+++ GenerateBibleMetrics/RunTests.bat
@@ -1,8 +1,22 @@
 @echo off
 if "%PYTHON_DIR%"=="" goto :error
 
+if "%1" == "" goto :allTests
+
 set PYTHONPATH=..\Output\release
-"%PYTHON_DIR%\python" GenerateBibleMetrics.py test
+"%PYTHON_DIR%\python" %1 test
+goto :end
+
+:allTests
+
+set PYTHONPATH=..\Output\release
+for %%i in (*.py) do call :runSpecificTest %%i
+goto :end
+
+:runSpecificTest
+echo.
+echo Running tests in %1
+"%PYTHON_DIR%\python" %1 test
 goto :end
 
 :error
